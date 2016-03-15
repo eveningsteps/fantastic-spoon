a90862b77a56454ea486674ce28d880b16f80bb7
eveningseps <eveningsteps@gmail.com>
2016-03-15 23:56:10 +0300

    move indexer to a separate class

--- ir-course/01-02-inverted-index/src/indexer.d
+++ ir-course/01-02-inverted-index/src/indexer.d
@@ -1,129 +1,114 @@
-import std.algorithm;
-import std.conv;
-import std.stdio;
 import std.file;
 import std.container;
-import std.datetime;
 import std.string;
-import std.getopt;
+import std.utf;
+import std.conv;
+import std.stdio;
 
 struct Doc
 {
 	int id, pos;
 }
 
-void main(string[] args)
-{	
-	string index_path, text_path;
-	getopt(args,
-		"i|index", &index_path,
-		"t|text", &text_path);
-	
-	if(!index_path.length || !text_path.length)
+class Indexer
+{
+	Doc[][dstring] index;
+	double[string] stats;
+
+	void build_index(dstring text)
 	{
-		writefln("USAGE: %s -i <index path> -t <text file>", args[0]);
-		return;
-	}
-
-	if(!exists(index_path))
-		mkdirRecurse(index_path);
-
-	File index = File(index_path ~ "/index", "wb");
-	File dictionary = File(index_path ~ "/dictionary", "w");
-
-	int token_id, term_id, doc_id;
-	long token_len, term_len;
-	auto delimiters = redBlackTree!dchar([' ', '.', ',', '"', '\'', '(', ')', '!', '?', ':', ';', '\n', '-']);
-	
-	Doc[][string] dict;
-
-	/*
-	
-	dictionary structure:
-		word -> position in index
-
-	index structure:
-		number of documents, then
-			for each document, its ID and position
-
-	*/
-
-	auto starttime = MonoTime.currTime();
-
-	string text = to!string(read(text_path));
-	string word = null;
-	int cur_token_start = 0, cur_doc_start = 0;
-	bool in_token = false;
-	
-	for(int j = 0; j < text.length; ++j)
-	{
-		char c = text[j];
-
-		// skip all tags, increment doc_id on </dd
-		if(c == '<')
-		{
-			string tag;
-			while(text[j] != '>')
-				tag ~= text[j++];
-			if(tag == "</dd")
-			{
-				++doc_id;
-				cur_doc_start = j + 1;
-			}
-
-			continue;
-		}
-
-		// convert token to term and add to dictionary
-		if(c == ' ' && !(word is null))
-		{
-			word = toLower(word);
-			
-			if(!(word in dict) || word in dict && dict[word][$ - 1].id != doc_id)
-				dict[word] ~= Doc(doc_id, cur_doc_start);
-
-			++token_id;
-			token_len += word.length;
-
-			word = null;
-			in_token = false;
-
-			continue;
-		}
-
-		// extend current token
-		if(!(c in delimiters))
-		{
-			if(!in_token)
-			{
-				cur_token_start = j;
-				in_token = true;
-			}
-			word ~= c;
-		}
-	}
-
-	term_id = to!int(dict.length);
-
-	int pos = 0;
-	dictionary.writeln(term_id);
-	foreach(term; dict.byKey())
-	{
-		term_len += term.length;
-		dictionary.writefln("%s %s %s", term, to!int(dict[term].length), pos);
+		int token_id, doc_id;
+		long token_len, term_len;
+		auto delimiters = redBlackTree!dchar([' ', '.', ',', '"', '\'', '(', ')', '!', '?', ':', ';', '\n', '-']);
 		
-		int d = to!int(dict[term].length);
-		index.rawWrite((&d)[0 .. 1]);
-		index.rawWrite(dict[term]);
+		dstring word = null;
+		int cur_token_start = 0, cur_doc_start = 0;
+		bool in_token = false;
+		
+		for(int j = 0; j < text.length; ++j)
+		{
+			dchar c = text[j];
 
-		pos += int.sizeof + dict[term].length * Doc.sizeof;
+			// skip all tags, increment doc_id on </dd
+			if(c == '<')
+			{
+				dstring tag;
+				while(text[j] != '>')
+					tag ~= text[j++];
+				if(tag == "</dd")
+				{
+					++doc_id;
+					cur_doc_start = j + 1;
+				}
+
+				continue;
+			}
+
+			// convert token to term and add to dictionary
+			if(c == ' ' && !(word is null))
+			{
+				word = toLower(word);
+				
+				bool first_occ = !(word in index);
+				bool next_occ = !first_occ && (index[word][$ - 1].id != doc_id);
+				
+				if(first_occ || next_occ)
+				{
+					if(first_occ)
+						term_len += word.length;
+					index[word] ~= Doc(doc_id, cur_doc_start);
+				}
+
+				++token_id;
+				token_len += word.length;
+
+				word = null;
+				in_token = false;
+
+				continue;
+			}
+
+			// extend current token
+			if(!(c in delimiters))
+			{
+				if(!in_token)
+				{
+					cur_token_start = j;
+					in_token = true;
+				}
+				word ~= c;
+			}
+		}
+
+		stats["tokens"] = token_id;
+		stats["terms"] = index.length;
+		stats["docs"] = doc_id;
+
+		stats["avg_token_length"] = token_len / to!double(token_id);
+		stats["avg_term_length"] = term_len / to!double(index.length);
 	}
 
-	auto endttime = MonoTime.currTime();
-	auto delta = endttime - starttime;
-	writefln("Execution time: %s ms", delta.total!"msecs");
-	writefln("Tokens: %s (average length: %s)", token_id, token_len / to!double(token_id));
-	writefln("Terms: %s (average length: %s)", term_id, term_len / to!double(term_id));
-	writefln("Read %s paragraphs of text", doc_id);
+
+	void save_index(string path)
+	{
+		if(!exists(path))
+			mkdirRecurse(path);
+
+		File index_file = File(path ~ "/index", "wb");
+		File dict_file = File(path ~ "/dictionary", "w");
+
+		int pos = 0;
+		dict_file.writeln(to!int(index.length));
+		foreach(term; index.byKey())
+		{
+			dict_file.writefln("%s %s %s", term, to!int(index[term].length), pos);
+			
+			int d = to!int(index[term].length);
+			index_file.rawWrite((&d)[0 .. 1]);
+			index_file.rawWrite(index[term]);
+
+			pos += int.sizeof + index[term].length * Doc.sizeof;
+		}
+	}
 }
 
---
+++ ir-course/01-02-inverted-index/src/main.d
@@ -0,0 +1,39 @@
+import std.conv;
+import std.stdio;
+import std.file;
+import std.datetime;
+import std.string;
+import std.getopt;
+import std.utf;
+
+import indexer;
+
+void main(string[] args)
+{	
+	string index_path, text_path;
+	getopt(args,
+		"i|index", &index_path,
+		"t|text", &text_path);
+	
+	if(!index_path.length || !text_path.length)
+	{
+		writefln("USAGE: %s -i <index path> -t <text file>", args[0]);
+		return;
+	}
+	
+	auto starttime = MonoTime.currTime();
+	auto indexer = new Indexer;
+	
+	string raw_text = to!string(read(text_path));
+	indexer.build_index(toUTF32(raw_text));
+	indexer.save_index(index_path);
+
+	auto endttime = MonoTime.currTime();
+	auto delta = endttime - starttime;
+	
+	writefln("Execution time: %s ms", delta.total!"msecs");
+	writefln("Tokens: %s (average length: %s)", indexer.stats["tokens"], indexer.stats["avg_token_length"]);
+	writefln("Terms: %s (average length: %s)", indexer.stats["terms"], indexer.stats["avg_term_length"]);
+	writefln("Read %s paragraphs of text", indexer.stats["docs"]);
+}
+
